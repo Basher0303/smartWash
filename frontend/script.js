76b8adefae7ee1877d94a00cfaa4a7f28bbdca16
Alexei Koshelev <aleksei.koshelev@onlyoffice.com>
2022-12-06 21:07:43 +0300

    Added style for button

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
     $('#start-btn').click(function () {
-        $.ajax({
-            type: "get",
-            url: "https://jsonplaceholder.typicode.com/posts",
-            dataType: "json",
-            success: function (response) {
-                console.log(response);
-            }
-        });        
+        request(1);       
     });
 });
+
+function request(status) {
+    $.ajax({
+        type: "post",
+        url: "http://localhost:5000/api/status",
+        dataType: "json",
+        timeout: 600000,
+        data: {
+            status: status,
+        },
+        success: function (response) {
+            console.log(response.status);
+            if(status != 5) 
+                request(response.status);
+        }
+    }); 
+}
