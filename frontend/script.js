d11ca64830c9ec2d303a6d2003744152ee9229bd
Alexei Koshelev <aleksei.koshelev@onlyoffice.com>
2022-12-16 02:54:53 +0300

    Added step-list

--- frontend/script.js
+++ frontend/script.js
@@ -1,29 +1,42 @@
 $(document).ready(function() {
     $('#start-btn').click(function () {
         $(this).attr('disabled', true);
-        changeIcon(1);
-        request(0);       
+        request();       
     });
 });
 
-function request(status) {
+function request() {
     $.ajax({
-        type: "post",
-        url: "http://localhost:5000/api/status",
+        type: "get",
+        url: "http://localhost:5000/api/start",
         dataType: "json",
         timeout: 600000,
-        data: {
-            status: status,
-        },
         success: function (response) {
             changeIcon(response.status);
-            console.log(response.status);
-            if(response.status != 5) 
+            changeStatusName(response.statusName);
+            if(response.status == 0) {
+                $('#start-btn').removeAttr('disabled');
+                $(`.step-list li`).removeClass('active');
+                $(`.step-list li`).removeClass('progress');
+            }
+            else {
                 request(response.status);
-        }
+                if(response.status > 1){
+                    $(`.step-list li:nth-child(${response.status-1})`).removeClass('progress');       
+                    $(`.step-list li:nth-child(${response.status-1})`).addClass('active');       
+                }         
+                $(`.step-list li:nth-child(${response.status})`).addClass('progress');
+            }
+        }   
     }); 
 }
 
 function changeIcon(status) {
     $('.icon').attr('src', `./src/status-${status}.png`);
 }
+
+function changeStatusName(name) {
+    $('#name-status').text(name);
+}
+
+
