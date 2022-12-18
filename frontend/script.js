5ab73ebaf42a2e2a0c52e1e1daf086fb2526aaf6
Alexei Koshelev <aleksei.koshelev@onlyoffice.com>
2022-12-18 18:13:09 +0300

    Added list status

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,58 @@
+const apiUrl = 'http://localhost:5000/api';
+const statusNames = ["Мойка свододна", "Нанесение эмульсии", "Нанесение пены", "Мойка", "Нанесение воска", "Сушка"];
+
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Нояб','Дек'];
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    date = date < 10 ? '0'.concat(date) : date;
+    var hour = a.getHours();
+    hour = hour < 10 ? '0'.concat(hour) : hour;
+    var min = a.getMinutes();
+    min = min < 10 ? '0'.concat(min) : min;
+    var time = `${date} ${month} ${hour}:${min}`;
+    return time;
+}
+
 $(document).ready(function() {
     $('#start-btn').click(function () {
         $(this).attr('disabled', true);
         request();       
+    });
+    $('#list-btn').click(function() {
+        $.ajax({
+            type: "get",
+            url: apiUrl + '/getList',
+            dataType: "json",
+            success: function (response) {
+                
+                console.log(response);
+                let addedHtml = '\
+                    <tr id="table-header">\
+                        <th>№</th>\
+                        <th>Действие</th>\
+                    <th>Дата</th>';
+                
+                response.forEach((element, index) => {
+                    addedHtml += `\
+                        <tr>\
+                            <td>${index+1}</td>\
+                            <td>${statusNames[element.status]}</td>\
+                            <td>${timeConverter(element.date)}</td>\
+                        </tr>`
+                });
+                $('#table').html(addedHtml);
+            }
+        });
     });
 });
 
 function request() {
     $.ajax({
         type: "get",
-        url: "http://localhost:5000/api/start",
+        url: apiUrl + '/start',
         dataType: "json",
         timeout: 600000,
         success: function (response) {
